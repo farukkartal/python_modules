f2e8bec0452a4a3d3ff936a1fa2a79f83757d760
faruk kartal <farukkartal@faruk-MacBook-Air-2.local>
2026-07-25 02:50:53 +0300

    main bloğu düzenlendi hesaplamalar fonksiyonlara ayrıldı

--- python module 03/ex2/ft_coordinate_system.py
+++ python module 03/ex2/ft_coordinate_system.py
@@ -27,23 +27,35 @@ def get_player_pos() -> tuple[float, float, float]:
                 return (x, y, z)
 
 
-if __name__ == "__main__":
+def distance_center(pos: tuple[float, float, float]) -> float:
+    return (math.sqrt(pos[0]**2 + pos[1]**2 + pos[2]**2))
+
+
+def distance_between(pos1: tuple[float, float, float],
+                     pos2: tuple[float, float, float]) -> float:
+    return (math.sqrt(
+        (pos2[0] - pos1[0])**2 +
+        (pos2[1] - pos1[1])**2 +
+        (pos2[2] - pos1[2])**2
+    ))
+
+
+def coordinate_system() -> None:
     print("=== Game Coordinate System ===\n")
     print("Get a first set of coordinates")
     pos1 = get_player_pos()
     print(f"Got a first tuple: {pos1}")
     print(f"It includes: X={pos1[0]}, Y={pos1[1]}, Z={pos1[2]}")
-    distance = math.sqrt(pos1[0]**2 + pos1[1]**2 + pos1[2]**2)
-    print(f"Distance to center: {distance:.4f}")
-    print()
+    distance = distance_center(pos1)
+    print(f"Distance to center: {distance:.4f}\n")
     print("Get a second set of coordinates")
     pos2 = get_player_pos()
-    dist_between = math.sqrt(
-        (pos2[0] - pos1[0])**2 +
-        (pos2[1] - pos1[1])**2 +
-        (pos2[2] - pos1[2])**2
-    )
+    dist_between = distance_between(pos1, pos2)
     print(
         "Distance between the 2 sets of coordinates: "
         f"{dist_between:.4f}"
     )
+
+
+if __name__ == "__main__":
+    coordinate_system()
