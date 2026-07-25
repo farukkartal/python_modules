b907f7dc331cee17c3b5072aad6c93a9c986d40d
faruk kartal <farukkartal@faruk-MacBook-Air-2.local>
2026-07-25 03:04:20 +0300

    main bloğu düzenlendi

--- python module 03/ex3/ft_achievement_tracker.py
+++ python module 03/ex3/ft_achievement_tracker.py
@@ -18,7 +18,7 @@ def gen_player_achievements() -> set[str]:
     return (set(selected_ach))
 
 
-if __name__ == "__main__":
+def track_achievements() -> None:
     print("=== Achievement Tracker System ===\n")
     alice_ach = gen_player_achievements()
     bob_ach = gen_player_achievements()
@@ -28,24 +28,28 @@ if __name__ == "__main__":
     print(f"Player Bob: {bob_ach}")
     print(f"Player Charlie: {charlie_ach}")
     print(f"Player Dylan: {dylan_ach}\n")
-    global_ach = alice_ach.union(bob_ach, charlie_ach, dylan_ach)
-    common_ach = alice_ach.intersection(bob_ach, charlie_ach, dylan_ach)
+    global_ach = set.union(alice_ach, bob_ach, charlie_ach, dylan_ach)
+    common_ach = set.intersection(alice_ach, bob_ach, charlie_ach, dylan_ach)
     print(f"All distinct achievements: {global_ach}\n")
     print(f"Common achievements: {common_ach}")
-    only_alice = alice_ach.difference(bob_ach, charlie_ach, dylan_ach)
-    only_bob = bob_ach.difference(alice_ach, charlie_ach, dylan_ach)
-    only_charlie = charlie_ach.difference(alice_ach, bob_ach, dylan_ach)
-    only_dylan = dylan_ach.difference(alice_ach, bob_ach, charlie_ach)
+    only_alice = set.difference(alice_ach, bob_ach, charlie_ach, dylan_ach)
+    only_bob = set.difference(bob_ach, alice_ach, charlie_ach, dylan_ach)
+    only_charlie = set.difference(charlie_ach, alice_ach, bob_ach, dylan_ach)
+    only_dylan = set.difference(dylan_ach, alice_ach, bob_ach, charlie_ach)
     print(f"Only Alice has: {only_alice}")
     print(f"Only Bob has: {only_bob}")
     print(f"Only Charlie has: {only_charlie}")
     print(f"Only Dylan has: {only_dylan}\n")
     full_ach_set = set(ALL_ACH)
-    alice_missing = full_ach_set.difference(alice_ach)
-    bob_missing = full_ach_set.difference(bob_ach)
-    charlie_missing = full_ach_set.difference(charlie_ach)
-    dylan_missing = full_ach_set.difference(dylan_ach)
+    alice_missing = set.difference(full_ach_set, alice_ach)
+    bob_missing = set.difference(full_ach_set, bob_ach)
+    charlie_missing = set.difference(full_ach_set, charlie_ach)
+    dylan_missing = set.difference(full_ach_set, dylan_ach)
     print(f"Alice is missing: {alice_missing}")
     print(f"Bob is missing: {bob_missing}")
     print(f"Charlie is missing: {charlie_missing}")
     print(f"Dylan is missing: {dylan_missing}")
+
+
+if __name__ == "__main__":
+    track_achievements()
