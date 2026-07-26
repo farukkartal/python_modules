a6b988ba904408831fbb0a8de1dba0083754169e
faruk kartal <farukkartal@faruk-MacBook-Air-2.local>
2026-07-26 03:10:55 +0300

    main bloğu fonksiyona taşındı

--- python module 03/ex6/ft_data_alchemist.py
+++ python module 03/ex6/ft_data_alchemist.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
-
 import random
 
 
-if __name__ == "__main__":
-    print("=== Game Data Alchemist ===")
+def run_alchemist() -> None:
+    print("=== Game Data Alchemist ===\n")
     players = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma',
                'Gregory', 'john', 'kevin', 'Liam']
     print(f"Initial list of players: {players}")
     capitalized_names = [name.capitalize() for name in players]
     print(f"New list with all names capitalized: {capitalized_names}")
     only_capitalized = [name for name in players if name.istitle()]
-    print(f"New list of capitalized names only: {only_capitalized}")
+    print(f"New list of capitalized names only: {only_capitalized}\n")
     scores = {name: random.randint(1, 1000) for name in capitalized_names}
     print(f"Score dict: {scores}")
-    average = sum(scores.values()) / len(scores)
+    total_score = sum(scores[name] for name in scores)
+    average = total_score / len(scores)
     rounded_avg = round(average, 2)
     print(f"Score average is {rounded_avg}")
-    high_scores = {name: score for name,
-                   score in scores.items() if score > average}
+    high_scores = {name: scores[name] for name in scores if
+                   scores[name] > average}
     print(f"High scores: {high_scores}")
+
+
+if __name__ == "__main__":
+    run_alchemist()
